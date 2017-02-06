ed8176d5aea374ea3ee4f3c2fc8763ee2c38cb8a
Arjunsingh <arjunsingh.y@amazatic.com>
2017-02-06 22:36:40 +0530

    tutorial 4

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.views.static import serve
 from django.conf import settings
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
 ]
 if not settings.DEBUG:
-    urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
+    urlpatterns += [url(r'^static/(?P<path>.*)$', serve,
+                        {'document_root': settings.STATIC_ROOT}), ]
